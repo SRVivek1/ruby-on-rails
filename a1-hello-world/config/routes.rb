60db7a81295a41725c2b0f3d0d63efca5ebf3c53
Vivek Singh Rajput <16172793+SRVivek1@users.noreply.github.com>
2023-08-07 00:53:52 +0530

    Hello World MVC
    Hello World MVC resources created and mapped

--- a1-hello-world/config/routes.rb
+++ a1-hello-world/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  # Map root URL to greeting#index
+  root "greeting#index"
+
+  # Map incoming Get reques to Greeting controller's index method.
+  get "/greet", to: "greeting#index"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
