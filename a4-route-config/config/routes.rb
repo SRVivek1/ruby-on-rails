1bbe814805721ba8df37cc6cc2d22b85fcee17d3
Vivek Singh Rajput <16172793+SRVivek1@users.noreply.github.com>
2023-08-09 00:28:19 +0530

    URL redirect with request params

--- a4-route-config/config/routes.rb
+++ a4-route-config/config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  get 'greeting/index'
+    
+  get '/greet', to: 'greeting#index'
+
+  # It will bind the value after '/greet/' to a variable 'user'
+  get '/greet/*user', to: 'greeting#index'
+
+  # Redirect to greet - ':user' will be read and sent to greet/*user
+  get '/hello/:user', to: redirect(status: 302, path: '/greet/%{user}')
+  
+
+
+  # Redirect to intenal url
+
+
+  # Redirect URLs with request params
+  get '/google/:query', to: redirect { |path_params, req| "https://google.com/search?q=#{path_params[:query].pluralize}" }
+  get '/stories/:name', to: redirect { |path_params, req| "/greet/#{path_params[:name].pluralize}" }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
